5eb877618f07759da627a90f95cb8a43b11f97d0
Simba14 <seanblundell@gmail.com>
2017-03-29 10:33:27 +0100

    Player 1 and 2 can enter names in form

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
 
-  get '/fighters-form' do
-    erb :index
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
     # start the server if ruby file executed directly
--- spec/features/user_story_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'User Stories' do
-  scenario 'Fighters see name on screen after filling in form' do
-    visit('/fighters-form')
-    fill_in('name', :with => 'John')
-    click_on('Submit')
-    expect(page).to have_content('John')
-  end
-end
