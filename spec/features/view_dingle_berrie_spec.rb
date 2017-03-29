1eb85ed7aa43d68d1fb72c2e272c7ca79bcb1888
Jack Bittiner <jackbittiner@hotmail.com>
2017-03-29 11:40:33 +0100

    sign_in_and_play webhelper added to tests

--- spec/features/view_dingle_berrie_spec.rb
+++ spec/features/view_dingle_berrie_spec.rb
@@ -1,9 +1,6 @@
 feature 'View fame points' do
   scenario 'see player 2\'s fame points' do
-  visit('/')
-  fill_in(:player_1_name, :with => 'John')
-  fill_in(:player_2_name, :with => 'Mittens')
-  click_button('Submit')
+  sign_in_and_play
   expect(page).to have_content('Mittens: 0 Fame Points')
   end
 end
