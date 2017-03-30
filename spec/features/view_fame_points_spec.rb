603d59a208195d89f3c15ee0c8b9951b177da7e2
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-03-30 12:50:31 +0100

    WIP: Rounded Mandys Corners

--- spec/features/view_fame_points_spec.rb
+++ spec/features/view_fame_points_spec.rb
@@ -1,6 +1,11 @@
 feature 'View fame points' do
   scenario 'see player 2\'s fame points' do
-  sign_in_and_play
-  expect(page).to have_content('Mittens: 100 Fame Points')
+    sign_in_and_play
+    expect(page).to have_content('Mittens: 100 Fame Points')
+  end
+
+  scenario 'see player 1\'s fame points' do
+    sign_in_and_play
+    expect(page).to have_content('John: 100 Fame Points')
   end
 end
