cc7f5f3744792effa1e301956869607ebac82aaa
Simba14 <seanblundell@gmail.com>
2017-03-30 14:53:50 +0100

    Players can now win and lose

--- spec/features/reduce_opponents_fp_spec.rb
+++ spec/features/reduce_opponents_fp_spec.rb
@@ -1,7 +1,20 @@
 feature "reducing opponents fame points" do
-  scenario "when player 1 attack, player 2's fp reduces" do
+  scenario "when player 1 attacks, player 2's fp reduces" do
     sign_in_and_play
     click_button('Do It')
     expect(page).to have_content("Mittens: 90fp")
+  end
+
+  scenario "when player 2 attacks, player 1's fp reduces" do
+    sign_in_and_play
+    click_button('Do It')
+    click_button('Retaliate')
+    click_button('Do It')
+    expect(page).to have_content("John: 90fp")
+  end
+
+  scenario "A player loses when their fame points reaches 0" do
+    full_on_fight
+    expect(page).to have_content "Jog on!"
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,12 @@ def sign_in_and_play
   fill_in(:player_2_name, :with => 'Mittens')
   click_button('Submit')
 end
+
+def full_on_fight
+  sign_in_and_play
+  18.times {
+    click_button('Do It')
+    click_button('Retaliate')
+  }
+  click_button('Do It')
+end
