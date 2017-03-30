25417dff461f7e026f6dd1050b6c9cb35f662b2c
Simba14 <seanblundell@gmail.com>
2017-03-30 12:01:58 +0100

    Refactored tests

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -2,6 +2,7 @@ feature 'switch player turn' do
 
   scenario 'player 1 begins the game' do
       sign_in_and_play
+      expect(page).not_to have_content("Mittens, its your turn to cause havoc!")
       expect(page).to have_content("John, its your turn to cause havoc!")
   end
 
@@ -9,6 +10,7 @@ feature 'switch player turn' do
     sign_in_and_play
     click_button('Do It')
     click_button('Retaliate')
+    expect(page).not_to have_content("John, its your turn to cause havoc!")
     expect(page).to have_content("Mittens, its your turn to cause havoc!")
   end
 
